fbe04a3933e2b7208be667cc54792eb75b0c7918
Leonardo Mera <leonardomera14@gmail.com>
2026-03-24 17:24:38 -0300

    new UX

--- app_streamlit.py
+++ app_streamlit.py
@@ -10,8 +10,10 @@ st.set_page_config(layout="wide")
 
 col1, col2, col3 = st.columns([1,2,1])
 
+with col1:
+    st.image("logo.png", width=100)
+
 with col2:
-    st.image("logo.png", width=150)
     st.markdown("<h2 style='text-align: center;'>Gerenciador de Imagens</h2>", unsafe_allow_html=True)
 
 # TABS (ESSA É A NAVEGAÇÃO CORRETA)
